86f52838d95ee7882cb80e167c0756246fe1a99c
inugasuki44 <iy4271729@gmail.com>
2026-05-31 15:22:11 +0900

    App-A,App-B2種類対応バージョンに更新

--- app/index.js
+++ app/index.js
@@ -3,8 +3,12 @@ const { HttpProxyAgent } = require('http-proxy-agent');
 const { HttpsProxyAgent } = require('https-proxy-agent');
 
 // 1. プロキシの指定
-// ※後で作成するDocker Composeで、Squidコンテナの名前を「proxy」とする前提です。
-const proxyUrl = 'http://proxy:3128';
+// ※Docker Composeで、Squidコンテナの名前をproxyとしている。
+// 変数はDocker Composeから参照している。
+const proxyUrl = process.env.HTTP_PROXY || 'http://proxy:3128';
+const targetUrl = process.env.TARGET_URL || 'http://httpbin.org/get';
+const appName = process.env.APP_NAME || 'App';
+
 const httpAgent = new HttpProxyAgent(proxyUrl);
 const httpsAgent = new HttpsProxyAgent(proxyUrl);
 
@@ -17,14 +21,15 @@ const client = axios.create({
 });
 
 async function checkCommunication() {
-  console.log('--- Egressプロキシ通信テストを開始します ---\n');
+  console.log('\n--- ${appName}のEgressプロキシ通信テストを開始します ---\n');
+  console.log('使用プロキシ:${proxyUrl}\n');
 
   await new Promise(resolve => setTimeout(resolve, 10000)); // ローカルでは3秒で機能したが、Actionsだと10秒くらい必要。
 
   // テスト1：許可されるはずの通信 (httpbin.org)
   try {
-    console.log('宛先: http://httpbin.org/get (許可リスト対象)');
-    const res1 = await client.get('http://httpbin.org/get');
+    console.log('宛先: ${targetUrl} (許可リスト対象)');
+    const res1 = await client.get('targetUrl');
     // HTTPステータスコード200が返ってきたら成功
     console.log(`✅ 結果: 通信成功 (Status: ${res1.status})`);
   } catch (error) {
